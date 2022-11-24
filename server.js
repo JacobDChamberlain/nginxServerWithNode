b0f2799e505b118a5b1699984737ba3f1d0127ca
Jacob Chamberlain <straightlegmusic@gmail.com>
2022-11-24 07:25:07 +0000

    FIXED CSS ISSUE - it was bc css is a static file and Express has a line for that

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    // res.send("Hello, World!");
-    // res.sendFile(path.join(__dirname, 'index.html'));
     const index = path.join(__dirname, 'index.html');
     res.sendFile(index);
 });
